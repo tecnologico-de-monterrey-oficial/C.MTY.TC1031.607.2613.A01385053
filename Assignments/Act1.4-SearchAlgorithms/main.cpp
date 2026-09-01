3916a3edaf888d0f9160db23efbc2cd9d220a860
Apullemanco <davidgil9@hotmail.com>
2026-08-31 23:16:46 -0600

    tarea lunes

--- Assignments/Act1.4-SearchAlgorithms/main.cpp
+++ Assignments/Act1.4-SearchAlgorithms/main.cpp
@@ -1,46 +1,76 @@
 // David Gil Alvarado
 // A01385053
+
 #include <iostream>
 #include <vector>
-#include <stdexcept>
+#include <algorithm>
+#include <ctime>
 
 using namespace std;
 
-int seqSearch(vector<int> &list, int data) {
-    for (int i=0; i<list.size(); i++) {
-        if (list[i] == data) {
+int busquedaSecuencial(vector<int> vec, int valor) {
+    for (int i = 0; i < vec.size(); i++) {
+        if (vec[i] == valor) {
             return i;
         }
     }
-    throw out_of_range("no se encontró el valor");
+    return -1;
 }
 
-template <typename T>
-int binarySearch(vector<T> &list, T data) {
-    int left = 0;
-    int right = list.size() - 1;
-    while (left <= right) {
-        int mid = (left + right) / 2;
-        if (data == list[mid]) {
-            return mid;
+int busquedaBinaria(vector<int> vec, int valor) {
+    int inicio = 0;
+    int fin = vec.size() - 1;
+
+    while (inicio <= fin) {
+        int medio = (inicio + fin) / 2;
+        if (vec[medio] == valor) {
+            return medio;
+        }
+        if (vec[medio] < valor) {
+            inicio = medio + 1;
         } else {
-            if (data < list[mid]) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
+            fin = medio - 1;
         }
     }
-    throw out_of_range("No se econtró el valor");
+    return -1;
 }
 
 int main() {
-    vector<int> list = {3, 5, 6, 8, 11, 12, 13, 16, 27, 35};
-    try {
-        int index = binarySearch(list, 14);
-        cout << "El valor se encuentra en: " << index << endl;
-    } catch (const out_of_range &e) {
-        cout << e.what() << endl;
+    srand(time(0));
+
+    vector<int> numeros(10000);
+    for (int i = 0; i < 10000; i++) {
+        numeros[i] = rand() % 1000000 + 1;
     }
+
+    sort(numeros.begin(), numeros.end());
+
+    int objetivo;
+    do {
+        cout << "Ingresa un numero entre 1 y 1,000,000 (0 para salir): ";
+        cin >> objetivo;
+
+        if (objetivo != 0) {
+            clock_t t1_sec = clock();
+            int idx_sec = busquedaSecuencial(numeros, objetivo);
+            clock_t t2_sec = clock();
+            double tiempo_sec = (double)(t2_sec - t1_sec) / CLOCKS_PER_SEC;
+
+            clock_t t1_bin = clock();
+            int idx_bin = busquedaBinaria(numeros, objetivo);
+            clock_t t2_bin = clock();
+            double tiempo_bin = (double)(t2_bin - t1_bin) / CLOCKS_PER_SEC;
+
+            if (idx_sec != -1) {
+                cout << "El numero SI se encuentra en la lista." << endl;
+            } else {
+                cout << "El numero NO se encuentra en la lista." << endl;
+            }
+
+            cout << "Tiempo busqueda secuencial: " << tiempo_sec << " segundos" << endl;
+            cout << "Tiempo busqueda binaria: " << tiempo_bin << " segundos" << endl << endl;
+        }
+    } while (objetivo != 0);
+
     return 0;
 }
